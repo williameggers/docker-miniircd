0ffc07307d44ea2892050bb6ac200b663d12ac1e
William Eggers <william@eggers.id.au>
2022-09-12 22:43:42 +1000

    Override bouncer buffer size using command line arg. Add additional env vars to Dockerfile. Add missing make jail commands to Dockerfile.

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,17 @@ FROM python:3-alpine
 
 USER root
 
-ADD https://raw.githubusercontent.com/jrosdahl/miniircd/master/miniircd /opt/miniircd/bin/miniircd
-
+#ADD https://raw.githubusercontent.com/bashrc2/miniircd/main/miniircd /opt/miniircd/bin/miniircd
+COPY miniircd /opt/miniircd/bin/miniircd
 COPY motd.txt /var/jail/miniircd/motd.txt
 
 RUN chmod 755 /opt/miniircd/bin/miniircd && \
-    chown -R nobody:nobody /var/jail/miniircd
+	mkdir -p /var/jail/miniircd/dev && \
+	chmod 755 /var/jail/miniircd && \
+	mknod /var/jail/miniircd/dev/null c 1 3 && \
+	mknod /var/jail/miniircd/dev/urandom c 1 9 && \
+	chmod 666 /var/jail/miniircd/dev/* && \
+    chown -R nobody:nobody /var/jail/miniircd 
 
 # Install OpenSSL
 RUN apk update && \
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,25 @@
 set -o errexit
 
 : ${MINIIRCD_SSL_ENABLED:=1}
+: ${MINIIRCD_CHANNEL_LOG_ENABLED:=1}
+: ${MINIIRCD_BOUNCER_BUFFER_SIZE:=512}
+: ${MINIIRCD_MAX_CLIENTS:=128}
+: ${MINIIRCD_NEW_REGISTRATIONS:=128}
+: ${MINIIRCD_MAX_CHANNELS:=8}
+: ${MINIIRCD_MAX_FLOOD_SCORE:=20}
+: ${MINIIRCD_CLOAK_HOST:=}
+
+MINIIRCD_VERSION=$(/opt/miniircd/bin/miniircd --version)
+MINIIRCD_OPT_ARGS=""
+
+if [[ ${MINIIRCD_CHANNEL_LOG_ENABLED} -eq 1 ]] ; then
+    MINIIRCD_OPT_ARGS="--channel-log-dir=/"
+fi
+
+if [ ! -z "${MINIIRCD_CLOAK_HOST}" ] ; then
+    echo "Using cloak hostname $MINIIRCD_CLOAK_HOST"
+    MINIIRCD_OPT_ARGS="$MINIIRCD_OPT_ARGS --cloak=$MINIIRCD_CLOAK_HOST "
+fi
 
 if [[ ${MINIIRCD_SSL_ENABLED} -eq 1 ]] ; then
     if [ ! -e "/var/jail/miniircd/ssl.pem" ] ; then
@@ -17,24 +36,34 @@ if [[ ${MINIIRCD_SSL_ENABLED} -eq 1 ]] ; then
         chown nobody:nobody /var/jail/miniircd/ssl.pem
     fi
 
-    echo "Starting SSL enabled miniircd."
+    echo "Starting SSL enabled miniircd (v$MINIIRCD_VERSION)."
     exec /opt/miniircd/bin/miniircd \
         --debug \
         --verbose \
+        --ports=6697 \
         --state-dir=/ \
-        --channel-log-dir=/ \
+        --bouncer-size=$MINIIRCD_BOUNCER_BUFFER_SIZE \
+        --max-clients=$MINIIRCD_MAX_CLIENTS \
+        --new-registrations=$MINIIRCD_NEW_REGISTRATIONS \
+        --max-flood-score=$MINIIRCD_MAX_FLOOD_SCORE \
+        --max-channels=$MINIIRCD_MAX_CHANNELS \
         --motd=/motd.txt \
         --setuid=nobody \
         --ssl-pem-file=/ssl.pem \
-        --chroot=/var/jail/miniircd
+        $MINIIRCD_OPT_ARGS --chroot=/var/jail/miniircd
 else
-    echo "Starting miniircd."
+    echo "Starting miniircd (v$MINIIRCD_VERSION)."
     exec /opt/miniircd/bin/miniircd \
         --debug \
         --verbose \
+        --ports=6667 \
         --state-dir=/ \
-        --channel-log-dir=/ \
+        --bouncer-size=$MINIIRCD_BOUNCER_BUFFER_SIZE \
+        --max-clients=$MINIIRCD_MAX_CLIENTS \
+        --new-registrations=$MINIIRCD_NEW_REGISTRATIONS \
+        --max-flood-score=$MINIIRCD_MAX_FLOOD_SCORE \
+        --max-channels=$MINIIRCD_MAX_CHANNELS \
         --motd=/motd.txt \
         --setuid=nobody \
-        --chroot=/var/jail/miniircd
+        $MINIIRCD_OPT_ARGS --chroot=/var/jail/miniircd
 fi
